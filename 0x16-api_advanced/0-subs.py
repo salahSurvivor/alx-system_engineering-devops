eed503fdf5c923168a0cf33ea0fcc88b6cc421c3
salahSurvivor <salaheddinenaji2000@gmail.com>
2024-03-06 11:54:46 -0800

    UPDATED

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,12 @@
 #!/usr/bin/python3
 """
-This module contains a function that queries the Reddit API and returns
-the number of subscribers for a given subreddit.
+0-main
 """
+import sys
 
-import requests
-
-def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers for the subreddit, or 0 if the subreddit is invalid.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'MyBot/0.0.1'}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
-        return 0
-
-if __name__ == "__main__":
-    import sys
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print(number_of_subscribers(sys.argv[1]))
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
